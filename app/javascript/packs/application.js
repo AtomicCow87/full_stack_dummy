7dbff46527554259b798ecbef887544202171d58
Zach <zkdoran@gmail.com>
2023-07-05 15:03:41 -0400

    page specific javascript

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,8 +8,9 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
-import "src/home"
-import "src/news"
+import "src/foo"
+//import "src/home"
+//import "src/news"
 
 Rails.start()
 Turbolinks.start()
